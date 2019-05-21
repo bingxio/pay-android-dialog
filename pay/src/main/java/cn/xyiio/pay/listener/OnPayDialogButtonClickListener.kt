7a46467dc3e8bd5911cdffbfb593cbcd35fb97ce
Turaiiao <31379103+Turaiiao@users.noreply.github.com>
2019-05-21 19:07:58 +0800

    Update OnPayDialogButtonClickListener.kt

--- pay/src/main/java/cn/xyiio/pay/listener/OnPayDialogButtonClickListener.kt
+++ pay/src/main/java/cn/xyiio/pay/listener/OnPayDialogButtonClickListener.kt
@@ -2,10 +2,11 @@ package cn.xyiio.pay.listener
 
 import android.app.AlertDialog
 import cn.xyiio.pay.PayDialog
+import cn.xyiio.pay.PayType
 
 interface OnPayDialogButtonClickListener {
 
-    fun onClickPayButton (dialog: AlertDialog)
+    fun onClickPayButton (dialog: AlertDialog, type: PayType)
 
     fun onClickCancelButton (dialog: AlertDialog, payDialogInstance: PayDialog)
 
